baa7519b8e23692a88f86cc5a0742afa211b610b
Adinarayana Reddy JC <jc.adi101@gmail.com>
2022-03-29 14:25:33 +0530

    Add project files.

---
+++ TravelAPP/TravelAPP/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelAPP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+
+           
+            if (string.IsNullOrEmpty(entryEmail.Text)|| string.IsNullOrEmpty(entryPWD.Text))
+            {
+
+            }
+            else
+            {
+
+                await Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
